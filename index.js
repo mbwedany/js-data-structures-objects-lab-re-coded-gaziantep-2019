f80ea13b1102c84257321550427001bb7cbe14f7
Mohamad Bwidani <mbwedany@gmail.com>
2019-07-24 11:04:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  const newDriver = Object.assign({}, driver)
+  const newDriver = Object.assign({}, driver);
+  newDriver [key] = value;
 }
